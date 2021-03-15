15e3d36a16ca23677c2c3967a03d48e45b62275a
王雨晨 <181250147@smail.nju.edu.cn>
2021-03-15 17:38:38 +0800

    feat: 图谱主题色

--- src/store/index.js
+++ src/store/index.js
@@ -20,9 +20,33 @@ const modules = modulesFiles.keys().reduce((modules, modulePath) => {
 const store = new Vuex.Store({
     state: {
          colorList: {
-            pink: {
-                key: '粉色', color: '#FFB8C6'
-            }
+             red: {
+                 key: '薄暮', color: '#F5222D'
+             },
+             orange: {
+                 key: '火山', color: '#FA541C'
+             },
+             yellow: {
+                 key: '日暮', color: '#FAAD14'
+             },
+             cyan: {
+                 key: '明青', color: '#13C2C2'
+             },
+             green: {
+                 key: '极光绿', color: '#52C41A'
+             },
+             blue: {
+                 key: '拂晓蓝', color: '#1890FF'
+             },
+             blue2: {
+                 key: '极客蓝', color: '#2F54EB'
+             },
+             purple: {
+                 key: '酱紫', color: '#722ED1'
+             },
+             pink: {
+                 key: '粉色（默认）', color: '#FFB8C6'
+             }
         }
     },
     modules:{
