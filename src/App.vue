5cf515946ffb8d642a90e1c15f6561f9c47c6f8c
王雨晨 <181250147@smail.nju.edu.cn>
2021-03-15 14:57:43 +0800

    fix: 组件设为中文环境

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <template>
-  <div id="app">
-<!--    <router-link to="/">Home</router-link>-->
-    <router-view/>
-  </div>
+  <a-config-provider :locale="zh_CN">
+    <div id="app">
+      <transition name="fade-transform" mode="out-in">
+        <router-view/>
+      </transition>
+    </div>
+  </a-config-provider>
 </template>
+
+<script>
+  import zh_CN from 'ant-design-vue/lib/locale-provider/zh_CN';
+
+  export default {
+    name: 'app',
+    methods:{
+
+    },
+    data() {
+      return {
+        zh_CN
+      };
+    }
+  };
+</script>
 
 <style>
 #app {
