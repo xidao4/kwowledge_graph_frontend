0acab72aad8d50ff9018213bed5edda47e1589cb
王雨晨 <181250147@smail.nju.edu.cn>
2021-03-29 09:01:49 +0800

    feat: 远程接口

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,7 +4,7 @@ import router from '../router'
 import { setToken, getToken } from '@/utils/auth'
 
 const service = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'http://118.182.96.49:8001': 'http://localhost:8001',
+  baseURL: process.env.NODE_ENV === 'production' ? 'http://118.182.96.49:8001': 'http://118.182.96.49:8001',
   timeout: 10 * 1000,
   withCredentials: true
 });
