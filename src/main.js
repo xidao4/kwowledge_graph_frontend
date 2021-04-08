499d0ad092cd4b1abde14af61dda94e1e57659a5
王雨晨 <181250147@smail.nju.edu.cn>
2021-04-08 11:39:25 +0800

    fix: 去掉不必要的moment依赖

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import moment from 'moment'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.less';
 import echarts from 'echarts'
@@ -12,9 +11,6 @@ import './mock'
 Vue.config.productionTip = false;
 
 Vue.use(Antd);
-
-Vue.prototype.$moment = moment;
-moment.locale('zh-cn');
 
 Vue.prototype.$echarts = echarts;
 
