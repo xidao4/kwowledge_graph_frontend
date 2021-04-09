2db3a8d49ef6259475a5ceaf6aef303ae96d77fe
刘佳月 <181250086@smail.nju.edu.cn>
2021-04-09 22:55:33 +0800

    feat: 筛选初步

---
+++ src/views/graph2.0/components/TypeFilter.vue
@@ -0,0 +1,69 @@
+<template>
+<div>
+    <p>类型筛选</p>
+    <ul>
+        <li v-for="(item,index) in checkboxList"
+            :key="item"
+            :class="checkbox.includes(index)?'active':''"
+            @click="check(index)">
+            {{item}}
+        </li>
+    </ul>
+
+</div>
+</template>
+
+<script>
+
+
+import {mapActions, mapGetters, mapMutations} from "vuex";
+
+export default {
+    name: "Filter",
+    components: {
+
+    },
+    data(){
+        return {
+            checkbox:[],
+            checkboxList:['cato1','cato2','cato2','cato3'],
+        }
+    },
+    computed: {
+        ...mapGetters([
+
+        ])
+    },
+    mounted(){
+
+    },
+    methods: {
+        ...mapMutations([]),
+        ...mapActions({
+
+        }),
+        check(i){
+            let idx=this.checkbox.indexOf(i);
+            if(idx>-1){
+                this.checkbox.splice(idx,1);
+            }else{
+                this.checkbox.push(i);
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+li{
+    display: inline-block;
+    font-size: 16px;
+    padding: 5px;
+    background-color: #e6e6e6;
+    margin: 5px 10px;
+    cursor: pointer;
+}
+.active {
+    border: 2px solid red;
+}
+</style>
