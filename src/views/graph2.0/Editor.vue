6c53e478ac328c7bbd146791465e571190ad6202
王雨晨 <181250147@smail.nju.edu.cn>
2021-04-14 15:48:00 +0800

    fix: 调色板超出范围

--- src/views/graph2.0/Editor.vue
+++ src/views/graph2.0/Editor.vue
@@ -1,6 +1,6 @@
 <template>
     <a-spin size="large" tip="加载编辑空间" :spinning="spinning">
-        <div style="padding-bottom: 20px; background-color: #EEEFF0; width: 100%">
+        <div style="padding-bottom: 150px; background-color: #EEEFF0; width: 100%">
             <Header></Header>
             <tool-bar></tool-bar>
             <a-row class="row">
