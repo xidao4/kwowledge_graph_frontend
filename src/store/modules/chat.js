c53581a377f0b99ee8b8992d7d9b141a05350574
181250178@smail.nju.edu.cn <181250178@smail.nju.edu.cn>
2021-06-16 23:44:23 +0800

    修改错别字

--- src/store/modules/chat.js
+++ src/store/modules/chat.js
@@ -6,7 +6,7 @@ const chat={
     roleSentence:[
       {name:'怡红院管家',firstSentence: '有什么想问的吗？再次点击右下角图标即可关闭对话框~',avatarUrl:'https://ydl8023.oss-cn-beijing.aliyuncs.com/20210614112137.png'},
       {name:'贾宝玉',firstSentence:'虽然未曾见过妹妹，但我看着面善，心里就算是旧相识，只作远别重逢。',avatarUrl: 'https://ydl8023.oss-cn-beijing.aliyuncs.com/20210614112223.png'},
-      {name:'林黛玉',firstSentence:'尔金死去侬收葬，未卜侬身何日丧？',avatarUrl: 'https://ydl8023.oss-cn-beijing.aliyuncs.com/20210614112437.png'},
+      {name:'林黛玉',firstSentence:'尔今死去侬收葬，未卜侬身何日丧？',avatarUrl: 'https://ydl8023.oss-cn-beijing.aliyuncs.com/20210614112437.png'},
       {name:'薛宝钗',firstSentence: '你有什么疑难，只管问我，我能解得，自然替你解一日。',avatarUrl: 'https://ydl8023.oss-cn-beijing.aliyuncs.com/20210614112523.png'},
       {name:'王熙凤',firstSentence: '哈哈哈哈哈......我来迟了，不曾迎接远客！',avatarUrl: 'https://ydl8023.oss-cn-beijing.aliyuncs.com/20210614112552.png'}
     ],
